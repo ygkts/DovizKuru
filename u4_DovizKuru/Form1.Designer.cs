61f30ad36c3573b65dce6f9bca2b92246a555634
Yasemin Göktaş <ygkts61@gmail.com>
2019-01-24 18:13:54 +0300

    ilk

---
+++ u4_DovizKuru/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace u4_DovizKuru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBirim = new System.Windows.Forms.Label();
+            this.lblSatis = new System.Windows.Forms.Label();
+            this.lblAlis = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(11, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(257, 340);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "YÜKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 496);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Birim :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 608);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Döv. Satış :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 553);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Döv. Alış :";
+            // 
+            // lblBirim
+            // 
+            this.lblBirim.AutoSize = true;
+            this.lblBirim.Location = new System.Drawing.Point(101, 494);
+            this.lblBirim.Name = "lblBirim";
+            this.lblBirim.Size = new System.Drawing.Size(20, 17);
+            this.lblBirim.TabIndex = 2;
+            this.lblBirim.Text = "...";
+            // 
+            // lblSatis
+            // 
+            this.lblSatis.AutoSize = true;
+            this.lblSatis.Location = new System.Drawing.Point(101, 608);
+            this.lblSatis.Name = "lblSatis";
+            this.lblSatis.Size = new System.Drawing.Size(20, 17);
+            this.lblSatis.TabIndex = 2;
+            this.lblSatis.Text = "...";
+            // 
+            // lblAlis
+            // 
+            this.lblAlis.AutoSize = true;
+            this.lblAlis.Location = new System.Drawing.Point(101, 553);
+            this.lblAlis.Name = "lblAlis";
+            this.lblAlis.Size = new System.Drawing.Size(20, 17);
+            this.lblAlis.TabIndex = 2;
+            this.lblAlis.Text = "...";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(295, 347);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 309);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(295, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(488, 309);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Döviz Ad";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Satış";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 676);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblAlis);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSatis);
+            this.Controls.Add(this.lblBirim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBirim;
+        private System.Windows.Forms.Label lblSatis;
+        private System.Windows.Forms.Label lblAlis;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ u4_DovizKuru/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Xml;
+
+namespace u4_DovizKuru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://www.tcmb.gov.tr/kurlar/today.xml"); // bu sayfa linkini xml olarak görmek için sayfada sağ tıkla sayfa kaynağını görüntüle diyerek ulaşabilirsin ;)
+            XmlElement rootEleman = xmlDoc.DocumentElement; // tüm döküman
+            XmlNodeList liste = rootEleman.GetElementsByTagName("Currency");    // currency = para birimi
+            List<Doviz> dlist = new List<Doviz>();
+            foreach (var item in liste)
+            {
+                Doviz d = new Doviz();
+                XmlElement currency = (XmlElement)item;
+                string isim = currency.GetElementsByTagName("Isim").Item(0).InnerText;
+
+                d.DovizAd = isim;
+                string alisFiyat = currency.GetElementsByTagName("ForexBuying").Item(0).InnerText;
+                string satisFiyat = currency.GetElementsByTagName("ForexSelling").Item(0).InnerText;
+                string birim = currency.GetElementsByTagName("Unit").Item(0).InnerText;
+
+                if (!string.IsNullOrEmpty(alisFiyat))
+                {
+                    d.AlisFiyat = Convert.ToDecimal(alisFiyat) / 10000;
+                }
+                if (!string.IsNullOrEmpty(satisFiyat))
+                {
+                    d.SatisFiyat = Convert.ToDecimal(satisFiyat) / 10000;
+                }
+                if (!string.IsNullOrEmpty(birim))
+                {
+                    d.Birim = Convert.ToInt32(birim);
+                }
+                listBox1.Items.Add(d);
+                dlist.Add(d);
+            }
+            dataGridView2.DataSource = dlist;
+            int i = 0;
+            foreach (var a in dlist)    // datagridview1 e 2 adet sütunu yazdırma
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = a.DovizAd;
+                dataGridView1.Rows[i].Cells[1].Value = a.SatisFiyat;
+                i++;
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Doviz secilenDoviz = (Doviz)listBox1.SelectedItem;
+            lblAlis.Text = secilenDoviz.AlisFiyat.ToString();
+            lblSatis.Text = secilenDoviz.SatisFiyat.ToString();
+            lblBirim.Text = secilenDoviz.Birim.ToString();
+        }
+
+    }
+}
